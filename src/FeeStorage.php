67192bb638dfbe9f1e546b7d19f0aa3e3a22c523
Jacob Embree <jacob.embree@stlouisintegration.com>
2019-12-31 10:21:28 -0500

    Pull upstream changes from commerce_promotion

--- src/FeeStorage.php
+++ src/FeeStorage.php
@@ -8,8 +8,10 @@ use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Cache\MemoryCache\MemoryCacheInterface;
 use Drupal\Core\Database\Connection;
-use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Entity\EntityFieldManagerInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Language\LanguageManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
@@ -33,21 +35,25 @@ class FeeStorage extends CommerceContentEntityStorage implements FeeStorageInter
    *   The entity type definition.
    * @param \Drupal\Core\Database\Connection $database
    *   The database connection to be used.
-   * @param \Drupal\Core\Entity\EntityManagerInterface $entity_manager
-   *   The entity manager.
+   * @param \Drupal\Core\Entity\EntityFieldManagerInterface $entity_field_manager
+   *   The entity field manager.
    * @param \Drupal\Core\Cache\CacheBackendInterface $cache
    *   The cache backend to be used.
    * @param \Drupal\Core\Language\LanguageManagerInterface $language_manager
    *   The language manager.
    * @param \Drupal\Core\Cache\MemoryCache\MemoryCacheInterface $memory_cache
    *   The memory cache.
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $entity_type_bundle_info
+   *   The entity type bundle info.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
    * @param \Symfony\Component\EventDispatcher\EventDispatcherInterface $event_dispatcher
    *   The event dispatcher.
    * @param \Drupal\Component\Datetime\TimeInterface $time
    *   The time.
    */
-  public function __construct(EntityTypeInterface $entity_type, Connection $database, EntityManagerInterface $entity_manager, CacheBackendInterface $cache, LanguageManagerInterface $language_manager, MemoryCacheInterface $memory_cache, EventDispatcherInterface $event_dispatcher, TimeInterface $time) {
-    parent::__construct($entity_type, $database, $entity_manager, $cache, $language_manager, $memory_cache, $event_dispatcher);
+  public function __construct(EntityTypeInterface $entity_type, Connection $database, EntityFieldManagerInterface $entity_field_manager, CacheBackendInterface $cache, LanguageManagerInterface $language_manager, MemoryCacheInterface $memory_cache, EntityTypeBundleInfoInterface $entity_type_bundle_info, EntityTypeManagerInterface $entity_type_manager, EventDispatcherInterface $event_dispatcher, TimeInterface $time) {
+    parent::__construct($entity_type, $database, $entity_field_manager, $cache, $language_manager, $memory_cache, $entity_type_bundle_info, $entity_type_manager, $event_dispatcher);
 
     $this->time = $time;
   }
@@ -59,10 +65,12 @@ class FeeStorage extends CommerceContentEntityStorage implements FeeStorageInter
     return new static(
       $entity_type,
       $container->get('database'),
-      $container->get('entity.manager'),
+      $container->get('entity_field.manager'),
       $container->get('cache.entity'),
       $container->get('language_manager'),
       $container->get('entity.memory_cache'),
+      $container->get('entity_type.bundle.info'),
+      $container->get('entity_type.manager'),
       $container->get('event_dispatcher'),
       $container->get('datetime.time')
     );
@@ -72,15 +80,16 @@ class FeeStorage extends CommerceContentEntityStorage implements FeeStorageInter
    * {@inheritdoc}
    */
   public function loadAvailable(OrderInterface $order) {
-    $today = gmdate('Y-m-d', $this->time->getRequestTime());
+    $date = $order->getCalculationDate()->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT);
+
     $query = $this->getQuery();
     $or_condition = $query->orConditionGroup()
-      ->condition('end_date', $today, '>=')
+      ->condition('end_date', $date, '>')
       ->notExists('end_date');
     $query
       ->condition('stores', [$order->getStoreId()], 'IN')
       ->condition('order_types', [$order->bundle()], 'IN')
-      ->condition('start_date', $today, '<=')
+      ->condition('start_date', $date, '<=')
       ->condition('status', TRUE)
       ->condition($or_condition);
     $result = $query->execute();
